3c195403a5ffb7cd7c425d72bb9e765a4704dace
Tosha <bapatta@mail.uc.edu>
2021-03-14 20:26:48 -0400

    added filter popup no function

---
+++ PetInsights_all/PetInsights_all/SearchViews/FilterModal.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+
+namespace PetInsights_all.SearchViews
+{
+    public partial class FilterModal : ContentPage
+    {
+        public FilterModal()
+        {
+            InitializeComponent();
+        }
+
+
+        void OnSelectionChange(object sender, CheckedChangedEventArgs args)
+        {
+            var chkbox = (CheckBox)sender;
+            Console.WriteLine(chkbox.ClassId);
+
+        }
+
+
+        async void OnDismissButtonClicked(object sender, EventArgs args)
+        {
+            Console.WriteLine(args);
+            await Navigation.PopModalAsync();
+        }
+    }
+}
