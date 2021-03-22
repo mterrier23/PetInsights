b999302a4cf3f533a4f9390252d8c5c1a558a073
Tosha <bapatta@mail.uc.edu>
2021-03-22 01:21:44 -0400

    pet breeds page and logo update

--- PetInsights_all/PetInsights_all/BreedsViews/MainBreeds.xaml.cs
+++ PetInsights_all/PetInsights_all/BreedsViews/MainBreeds.xaml.cs
@@ -15,6 +15,21 @@ namespace PetInsights_all
         public MainBreeds()
         {
             InitializeComponent();
+            transparentColor0.BackgroundColor = new Color(1, 1, 1, 0.8);
+            transparentColor1.BackgroundColor = new Color(1, 1, 1, 0.7);
+            transparentColor2.BackgroundColor = new Color(1, 1, 1, 0.7);
+            transparentColor3.BackgroundColor = new Color(1, 1, 1, 0.7);
+            transparentColor4.BackgroundColor = new Color(1, 1, 1, 0.7);
+            transparentColor5.BackgroundColor = new Color(1, 1, 1, 0.7);
+            transparentColor6.BackgroundColor = new Color(1, 1, 1, 0.7);
+            transparentColor7.BackgroundColor = new Color(1, 1, 1, 0.7);
+            transparentColor8.BackgroundColor = new Color(1, 1, 1, 0.7);
+            transparentColor9.BackgroundColor = new Color(1, 1, 1, 0.7);
+            transparentColor10.BackgroundColor = new Color(1, 1, 1, 0.7);
+            transparentColor11.BackgroundColor = new Color(1, 1, 1, 0.7);
+            transparentColor12.BackgroundColor = new Color(1, 1, 1, 0.7);
+
+
         }
     }
 }
